792363519cbedee0745f3c2485961bf02a868d3a
bodhello <bodhi.alarcon@gmail.com>
2018-09-15 20:01:00 -0400

    Complete Factorial Exercise

--- src/main/java/com/thoughtworks/tddintro/exercises/factorial/Factorial.java
+++ src/main/java/com/thoughtworks/tddintro/exercises/factorial/Factorial.java
@@ -1,7 +1,16 @@
 package com.thoughtworks.tddintro.exercises.factorial;
 
 public class Factorial {
+
     public Integer compute(int i) {
-        return -1;
+        if (i < 0){
+            throw new IllegalArgumentException("Negative numbers not allowed.");
+        }
+
+        int result = 1;
+        for (int j = result; j <= i; ++j) {
+            result *= j;
+        }
+        return result;
     }
 }
